2e6fe1e63a5b2ca1939abe1dbabff84cdb338042
Anjali-Chitkara-ac <69366199+Anjali-Chitkara-ac@users.noreply.github.com>
2021-09-21 20:01:21 -0700

    table view controller example

---
+++ TableViewControllerExample/TableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  TableViewController.swift
+//  TableViewControllerExample
+//
+//  Created by Anjali Chitkara on 9/16/21.
+//
+
+import UIKit
+
+class TableViewController: UITableViewController {
+    
+    let arr = ["a","b","c","d"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return arr.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+
+        // Configure the cell...
+        cell.textLabel?.text = arr[indexPath.row]
+
+        return cell
+    }
+
+}
